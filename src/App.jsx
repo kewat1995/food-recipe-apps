30f60ff853c3adaa8aef95afab739235406409ac
kewat1995 <surendra121030@gmail.com>
2024-02-09 16:17:11 +0000

    no change

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom";
 import "./App.css";
-import "bootstrap/dist/css/bootstrap.css"
+import "bootstrap/dist/css/bootstrap.min.css"
 import Navbar from "./components/Navbar";
 import FoodRecipeProvider from "./context";
 
